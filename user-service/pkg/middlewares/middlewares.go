c39dc46859a5f24b0084d4fbe311b134f40dd7ba
vizitiuRoman <romanv@readcast.net>
2020-06-13 18:42:43 +0300

    Save token in redis

--- user-service/pkg/middlewares/middlewares.go
+++ user-service/pkg/middlewares/middlewares.go
@@ -1,10 +1,41 @@
 package middlewares
 
-import "net/http"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	. "github.com/devs-backend/user-service/pkg/auth"
+	. "github.com/devs-backend/user-service/pkg/utils"
+)
 
 func MiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		next(w, r)
+	}
+}
+
+func MiddlewareAUTH(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		token, err := EncodeToken(r)
+		if err != nil {
+			ERROR(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
+			return
+		}
+
+		_, err = GetToken(&AccessDetails{
+			AccessUUID: token.AccessUUID,
+			UserID:     token.UserID,
+		})
+		if err != nil {
+			ERROR(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
+			return
+		}
+
+		fmt.Println("Q")
+
 		next(w, r)
 	}
 }
